1f4b1bf6e1dd517fa6c2a2d481672e06faf1aa9d
Fábio Santos <fabiosantosart@gmail.com>
2013-11-13 02:34:17 +0000

    first draft of a compiler, require() hook. Experimental compiler up

---
+++ compiler.js
@@ -0,0 +1,61 @@
+module.exports = { compile: compile };
+
+var falafel = require('falafel');
+
+var isKeyword = {
+  isKeyword: function findEnvDecls (id) {
+    return id === 'env' ? true : undefined ;
+  }
+};
+
+function precompile(source) {
+    return source.replace(/env\s+(\S+)\s*?=(?!=)/gm, function (_, $1) {
+      return '$ENV.' + $1 + ' = ';
+    })
+}
+
+function postWrap(source) {
+  return (
+    '(function ($ENV) {' +
+    source +
+    '}(require(\'env.js\').create(/* root env */)))'
+  );
+}
+
+function onNode(node) {
+  var type = node.type;
+  if (type === 'FunctionExpression') {
+    var src = node.source()
+        .replace(/^function.*?\(/, 'function ($ENV, ')
+        .replace(/^function \(\$ENV,\s+\)/, 'function ($ENV)')  // zero-argument case
+    node.update('env.bindLexical($ENV, ' + src + ')');
+  } else if (type === 'CallExpression') {
+    var argSrc =
+      node.arguments.map(function (n) { return n.source(); }).join(', ');
+    if (argSrc.length > 0) { argSrc = ', ' + argSrc }
+    if (node.callee.type === 'MemberExpression') {
+      node.update('env.callMethod($ENV, ' + node.callee.object.source()
+        + ', "' + node.callee.property.name + '"' + argSrc + ')');
+    } else {
+      node.update('env.call($ENV, ' + node.callee.source() + argSrc + ')');
+    }
+  } else if (type === 'UnaryExpression' && node.keyword === 'env') {
+    node.update('$ENV.' + node.argument.source());
+  } 
+}
+
+function compile(codeStr) {
+  codeStr = precompile(codeStr);
+  var compiled = falafel(codeStr, isKeyword, onNode);
+  return postWrap(compiled + '');
+}
+
+// Direct execution
+if (module.parent === null) {
+  process.stdout.write(compile(
+    require('fs').readFileSync(
+      process.argv[2],
+      { encoding: 'utf8' })));
+}
+
+/* vim: set sw=2 sts=2 et: */
---
+++ requireHook.js
@@ -0,0 +1,21 @@
+
+var compiler = require('./compiler.js');
+var Module = require('module').Module;
+
+var utf8 = { encoding: 'utf-8' };
+
+module.exports = function addRequireHook() {
+  var oldExt = require.extensions['.js'];
+  require.extensions['.js'] = function (module, filename) {
+    if (/\.env\.js$/.test(filename)) {
+      var code = compiler.compile(fs.readFileSync(filename, utf8));
+      var filename = '.env.' + filename.replace(/\.env\.js$/, '.js');
+      fs.writeFileSync(outName, code, utf8);
+    }
+    return oldExt(module, filename);
+  }
+}
+
+module.exports()
+
+/* vim: set sw=2 sts=2 et: */
