af7b24759096257f777f48db52a9c8a875f366f7
Fábio Santos <fabiosantosart@gmail.com>
2014-06-05 11:57:52 +0100

    add bin/envjs

--- requireHook.js
+++ requireHook.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 
 var utf8 = { encoding: 'utf-8' };
 
+module.exports =
 function addRequireHook() {
   var oldExt = require.extensions['.js'];
   require.extensions['.js'] = function (module, filename) {
@@ -21,16 +22,5 @@ function addRequireHook() {
     return oldExt(module, filename);
   }
 }
-
-if (module.parent === null) {
-  var path = require('path');
-
-  addRequireHook();
-
-  var fname = path.resolve(process.argv[2], process.env.PWD, process.argv[2]);
-  require(fname);
-}
-
-module.exports = addRequireHook;
 
 /* vim: set sw=2 sts=2 et: */
